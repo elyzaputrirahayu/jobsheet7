86c17a639dc69dc759b333c47d8eac628606c1a6
elyzaputrirahayu <eliscantik2000@gmail.com>
2025-10-21 19:50:51 +0700

    deklarasi variabel dan inisialisasi

--- SiakadFor08.java
+++ SiakadFor08.java
@@ -4,6 +4,6 @@ public class SiakadFor08 {
 
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
-
+        double nilai, tertinggi = 0, terendah = 100;
     }
 }
