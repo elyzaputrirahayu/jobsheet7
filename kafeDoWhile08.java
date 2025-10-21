4692447bdc6c4bc6cb74f7de9fff09e9c00eee41
elyzaputrirahayu <eliscantik2000@gmail.com>
2025-10-22 01:20:27 +0700

    didalam DO-while menambahkan perintah dan menambah kondisi if

--- kafeDoWhile08.java
+++ kafeDoWhile08.java
@@ -3,12 +3,29 @@ import java.util.Scanner;
 public class kafeDoWhile08 {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
+        
+        int Kopi, Teh, Roti, totalHarga;
+        int hargaKopi = 12000, hargaTeh = 7000, hargaRoti = 20000;
 
-        int kopi = 12000, teh = 7000, roti = 20000;
         String namaPelanggan;
 
         do {
+            System.out.println("masukkan nama pelanggan (ketik 'batal' untuk keluar)");
+            namaPelanggan = sc.nextLine();
+            if (namaPelanggan.equalsIgnoreCase("batal")) {
+                System.out.println("Transaksi dibatalkan");
+                break;
+            }
+            System.out.println("jumlah kopi :");
+            Kopi = sc.nextInt();
+            System.out.println("jumlah teh :");
+            Teh = sc.nextInt();
+            System.out.println("jumlah roti :");
+            Roti = sc.nextInt();
 
+            totalHarga = (Kopi*hargaKopi) + (Teh*hargaTeh) + (Roti* hargaRoti);
+            System.out.println("Total yang harus dibayar : Rp" + totalHarga);
+            sc.nextLine();
         } while(true);
     }
 }
