6fdc09ef5444e06c4c7a85ae5805a68f051b2fb9
elyzaputrirahayu <eliscantik2000@gmail.com>
2025-10-21 21:30:50 +0700

    membuat deklarasi dan inisialisasi, menulis kode program untuk menerima input

---
+++ SiakadWhile08.java
@@ -0,0 +1,14 @@
+import java.util.Scanner;
+
+public class SiakadWhile08 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        int nilai, jml, i = 0;
+
+        System.out.println("masukkan jumlah mahasiswa :");
+        jml = sc.nextInt();
+        
+    }
+    
+}
