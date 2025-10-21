5a190e9f70a936102874faf896bc3c1d098a8ed7
elyzaputrirahayu <eliscantik2000@gmail.com>
2025-10-21 23:02:24 +0700

    membuat deklarasi dan inisialisasi

---
+++ kafeDoWhile08.java
@@ -0,0 +1,10 @@
+import java.util.Scanner;
+
+public class kafeDoWhile08 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        int kopi = 12000, teh = 7000, roti = 20000;
+        String namaPelanggan;
+    }
+}
