1234479e0bb60705e4406066c221562738d83d32
elyzaputrirahayu <eliscantik2000@gmail.com>
2025-10-21 22:18:14 +0700

    membuat kondisi if, kemudian menambahkan if-else-if-else

--- SiakadWhile08.java
+++ SiakadWhile08.java
@@ -10,6 +10,29 @@ public class SiakadWhile08 {
         jml = sc.nextInt();
 
         while (i < jml) {
+            System.out.println("masukkan nilai mahasiswa ke-" +(i+1) +":");
+            nilai = sc.nextInt();
+
+            if (nilai < 0 || nilai > 100) {
+                System.out.println("Nilai tidak valid. masukkan lagi nilai yang valid!");
+                continue;
+            }
+
+            if (nilai > 80 && nilai <= 100) {
+                System.out.println("nilai mahasiswa ke-" + (i+1) +"adalah A");
+            } else if (nilai > 73 && nilai <= 80) {
+                System.out.println("nilai mahasiswa ke-" + (i+1) + "adalah B+");
+            } else if (nilai > 65 && nilai <= 73) {
+                System.out.println("nilai mahasiswa ke-" + (i+1) + "adalah B");
+            } else if (nilai > 60 && nilai <= 65) {
+                System.out.println("nilai mahasiswa ke-" + (i+1) + "adalah C+");
+            } else if (nilai > 50 && nilai <= 60) {
+                System.out.println("nilai mahasiswa ke-" + (i+1) + "adalah C");
+            } else if (nilai > 39 && nilai <= 50) {
+                System.out.println("nilai mahasiswa ke-" + (i+1) + "adalah D");
+            } else {
+                System.out.println("nilai mahasiswa ke-" +(i+1) +"adalah E");
+            }
             i++;
         }
 
