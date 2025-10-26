9199940808b50a7c7e3029553cac41edae2d69f4
elyzaputrirahayu <eliscantik2000@gmail.com>
2025-10-26 23:45:20 +0700

    menambahkan while

--- PenjualanTiketBioskop.java
+++ PenjualanTiketBioskop.java
@@ -4,7 +4,16 @@ public class PenjualanTiketBioskop {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
 
+        final int hargaTiket = 50000;
+        int jumlahTiket;
+        double totalHarga;
+        int totalTiketTerjual = 0;
+        double totalPenjualan = 0;
+
+        while (true) {
+            System.out.print("Masukkan jumlah tiket yang dibeli (ketik -1 untuk selesai): ");
+            jumlahTiket = sc.nextInt();
         
     }
-}
 
+}
