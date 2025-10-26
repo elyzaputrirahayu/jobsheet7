cef1b2e13cded37d342cfeb11c4a7d280df1576d
elyzaputrirahayu <eliscantik2000@gmail.com>
2025-10-27 06:20:53 +0700

    membuat bagian input

--- parkir.java
+++ parkir.java
@@ -2,6 +2,8 @@ import java.util.Scanner;
     public class parkir {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
+        int jenis, durasi;
+        int total = 0;
 
        
     }
